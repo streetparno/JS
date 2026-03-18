5b75eaf759c7c056fd6d0715e55b60952adba3a1
streetparno <matyas.bruder1@gmail.com>
2026-03-18 11:36:03 +0100

    liuézřžtf
    zzzz

---
+++ 3.3.5/script.JS
@@ -0,0 +1,27 @@
+let podminka = !false;
+
+podminka = true && true;
+
+podminka = false || true;
+
+console.log(podminka)
+
+// Podmínka pře if
+
+let number = 10;
+let message;
+
+if (number > 0) {
+    message = "Kladné číslo";
+} else {
+    message = "Není kladné";
+}
+
+console.log(message);
+
+// Druhé řešení pomocí ternárního operátoru
+
+let number2 = 10;
+let message2 = (number2 > 0) ? "Kladné číslo" : "Není kladné";
+
+console.log(message2)
---
+++ cvičení 3.3.2/JS script.js
@@ -0,0 +1,15 @@
+let username= "Jan";
+console.log(username)
+
+username = "Petra";
+console.log(username)
+
+let agr = 21;
+console.log(age)
+let age2 = 25;
+console.log(age2)
+
+console.log(userage1)
+console.log(userage)
+let text = "Text"
+console.log(typeof text)
